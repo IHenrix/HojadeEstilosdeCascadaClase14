b2f59baa845c9ca3254966fb1a3c4a59771bd4fc
Enrique Prada <enrique.pdg@gmail.com>
2026-06-29 23:30:34 -0500

    Revertir conexion.php al estado original

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,9 +1,9 @@
 <?php
-$host   = $_ENV['PGHOST']     ?? $_SERVER['PGHOST']     ?? 'localhost';
-$puerto = $_ENV['PGPORT']     ?? $_SERVER['PGPORT']     ?? 5432;
-$dbname = $_ENV['PGDATABASE'] ?? $_SERVER['PGDATABASE'] ?? 'railway';
-$user   = $_ENV['PGUSER']     ?? $_SERVER['PGUSER']     ?? 'postgres';
-$pass   = $_ENV['PGPASSWORD'] ?? $_SERVER['PGPASSWORD'] ?? '';
+$host   = $_ENV['PGHOST']     ?? 'localhost';
+$puerto = $_ENV['PGPORT']     ?? 5432;
+$dbname = $_ENV['PGDATABASE'] ?? 'bd_utp';
+$user   = $_ENV['PGUSER']     ?? 'postgres';
+$pass   = $_ENV['PGPASSWORD'] ?? '1234';
 
 $dsn  = "pgsql:host=$host;port=$puerto;dbname=$dbname";
 $conn = new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
