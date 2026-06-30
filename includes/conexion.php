7b33ec097ac2c910d8084b38223dd750fea3389e
Enrique Prada <enrique.pdg@gmail.com>
2026-06-29 23:30:10 -0500

    Fix: leer variables con  y

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,9 +1,9 @@
 <?php
-$host   = getenv('PGHOST')     ?: 'localhost';
-$puerto = getenv('PGPORT')     ?: 5432;
-$dbname = getenv('PGDATABASE') ?: 'railway';
-$user   = getenv('PGUSER')     ?: 'postgres';
-$pass   = getenv('PGPASSWORD') ?: '1234';
+$host   = $_ENV['PGHOST']     ?? $_SERVER['PGHOST']     ?? 'localhost';
+$puerto = $_ENV['PGPORT']     ?? $_SERVER['PGPORT']     ?? 5432;
+$dbname = $_ENV['PGDATABASE'] ?? $_SERVER['PGDATABASE'] ?? 'railway';
+$user   = $_ENV['PGUSER']     ?? $_SERVER['PGUSER']     ?? 'postgres';
+$pass   = $_ENV['PGPASSWORD'] ?? $_SERVER['PGPASSWORD'] ?? '';
 
 $dsn  = "pgsql:host=$host;port=$puerto;dbname=$dbname";
 $conn = new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
